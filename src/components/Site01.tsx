576ce4e6173b6fc6e353b38fe509f81d0b588f55
CleidsonGoes <jorgegoes4@gmail.com>
2024-10-08 00:41:44 -0300

    segunda section em produção e estilização

--- src/components/Site01.tsx
+++ src/components/Site01.tsx
@@ -1,4 +1,5 @@
 import './Site01.css'
+import img01 from '../assets/img01.jpg'
 
 function SiteOne() {
   return(
@@ -19,6 +20,17 @@ function SiteOne() {
             <h2>Always Choose Good</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel, iusto perspiciatis consectetur necessitatibus doloribus labore expedita facilis repellendus hic dignissimos perferendis minima deleniti unde eligendi vero qui, excepturi eum?</p>
             <a href="#" className="btn">Our Menu</a>
+        </div>
+      </section>
+      <section className='about' id='about'>
+        <div className='row'>
+          <div className='col50'>
+            <h2 className='titleText'><span>A</span>bout Us</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni sunt, impedit distinctio ab dolorem quam a deleniti, quod sed pariatur debitis nemo laborum quo voluptatibus consequuntur dolore labore tenetur inventore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vitae dignissimos praesentium quos perferendis non repudiandae reiciendis unde! Harum deleniti architecto in amet iure fuga doloremque odio nam sequi maxime!<br></br><br></br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, labore obcaecati eius reiciendis dolores iure voluptatibus cupiditate officia minus adipisci. Fugiat nisi accusantium minus hic, consequuntur a doloremque veniam cum.</p>
+          </div>
+          <div className='imgBx'>
+            <img src={img01} alt='imagem 01'/>
+          </div>
         </div>
       </section>
       <script type="text/javascript">
